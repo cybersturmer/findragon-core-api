9a797b9261240dbdf42b9b237c1f7c6c40605f66
Vladimir Shturmer <cybersturmer@ya.ru>
2021-12-05 22:14:06 +0300

    Additional models added.

--- tables.py
+++ tables.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Unicode
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Unicode, Date, Float
 from sqlalchemy_utils.types import ChoiceType
 from sqlalchemy_utils import Currency, CurrencyType
 
@@ -12,13 +12,75 @@ class AllocationType(Enum):
     CURRENCY = auto()
 
 
+class TransactionType(Enum):
+    BUY = auto()
+    SELL = auto()
+
+
+class BrokerType(Enum):
+    SBER = auto()
+    TCS = auto()
+
+
+class PortfolioGoalType(Enum):
+    GROW = auto()
+    INCOME = auto()
+
+
+class PaymentType(Enum):
+    DIVIDENDS = auto()
+    COUPON = auto()
+
+
+class AssetType(Enum):
+    SHARE = auto()
+    BOND = auto()
+    FUND = auto()
+
+
+class PortfolioSettingsModel(Base):
+    __tablename__ = 'portfolio_settings'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+        autoincrement=True
+    )
+
+    apply_taxes_on_paid_dividends = Column(
+        Boolean
+    )
+
+    tax_percent = Column(
+        Integer
+    )
+
+    broker = Column(
+        ChoiceType(BrokerType, impl=Integer())
+    )
+
+    goal_currency = Column(
+        String
+    )
+
+    goal_type = Column(
+        ChoiceType(PortfolioGoalType, impl=Integer())
+    )
+
+    goal_value = Column(
+        Integer
+    )
+
+
 class AllocatedPieItemModel(Base):
     __tablename__ = "allocated_pie_items"
 
     id = Column(
         Integer,
         primary_key=True,
-        index=True
+        index=True,
+        autoincrement=True
     )
 
     type = Column(
@@ -58,3 +120,103 @@ class AllocatedPieItemModel(Base):
     )
 
 
+class PortfolioTransactionModel(Base):
+    __tablename__ = 'portfolio_transactions'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+        autoincrement=True
+    )
+
+    amount = Column(
+        Integer
+    )
+
+    asset_type = Column(
+        ChoiceType(AssetType, impl=Integer())
+    )
+
+    imported = Column(
+        Boolean
+    )
+
+    commission = Column(
+        Float
+    )
+
+    commission_currency = Column(
+        String
+    )
+
+    date = Column(
+        Date
+    )
+
+    description = Column(
+        Unicode(255)
+    )
+
+    title = Column(
+        Unicode(55)
+    )
+
+    accrued_interest = Column(
+        Float
+    )
+
+    price = Column(
+        Float
+    )
+
+    ticker = Column(
+        String
+    )
+
+    exchange = Column(
+        String
+    )
+
+    total_price = Column(
+        Float
+    )
+
+    type = Column(
+        ChoiceType(TransactionType, impl=Integer())
+    )
+
+
+class InvestmentModel(Base):
+    __tablename__ = 'portfolio_investment'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+        autoincrement=True
+    )
+
+    amount = Column(
+        Float
+    )
+
+    buy_value = Column(
+        Float
+    )
+
+    currency = Column(
+        String
+    )
+
+    description = Column(
+        Unicode(255)
+    )
+
+    exchange = Column(
+        String
+    )
+
+    ticker = Column(
+        String
+    )
