30fc73907a0628d8cd622b6f8febdacb2cd3cf71
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-16 01:29:41 +0300

    Additional router for income endpoints added.

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from .allocations import router as allocation_router
 from .portfolios import router as portfolio_router
 from .assets import router as asset_router
 from .transactions import router as transaction_router
+from .income import router as income_router
 
 from .eod import router as eod_router
 
@@ -12,7 +13,8 @@ router = APIRouter()
 router.include_router(allocation_router)
 router.include_router(portfolio_router)
 router.include_router(transaction_router)
-
+router.include_router(income_router)
 router.include_router(asset_router)
 
+# Just to make POC for EOD integration.
 router.include_router(eod_router)
