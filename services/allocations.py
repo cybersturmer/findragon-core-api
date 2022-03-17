47f44d77ef253b2a598ca5a3bcf1294ffd69019f
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-18 01:02:35 +0300

    Let's rollback this stupid commit.

--- services/allocations.py
+++ services/allocations.py
@@ -46,7 +46,6 @@ class Allocation:
 
         allocation_type = allocation_data['type']
         portfolio_id = allocation_data['portfolio_id']
-        description = f'{ticker}.{exchange}'
 
         if allocation_type == AllocationType.ASSET:
             asset = (
@@ -55,7 +54,6 @@ class Allocation:
                 .filter_by(
                     ticker=ticker,
                     exchange=exchange,
-                    description=description,
                     portfolio_id=portfolio_id
                 )
                 .first()
--- services/incomes.py
+++ services/incomes.py
@@ -53,7 +53,6 @@ class Income:
         exchange = income_data.pop('exchange')
 
         portfolio_id = income_data['portfolio_id']
-        description = f'{ticker}.{exchange}'
 
         asset = (
             self.orm_session
@@ -61,7 +60,6 @@ class Income:
             .filter_by(
                 ticker=ticker,
                 exchange=exchange,
-                description=description,
                 portfolio_id=portfolio_id
             )
             .first()
