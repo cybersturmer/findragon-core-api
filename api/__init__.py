a75a5182b28b8551a0580f64327b7ef568cf8eee
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-22 01:55:51 +0300

    Dashboard added to api root.

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ from .portfolios import router as portfolio_router
 from .assets import router as asset_router
 from .transactions import router as transaction_router
 from .incomes import router as income_router
+from .dashboard import router as dashboard_router
 
 from .eod import router as eod_router
 
@@ -15,6 +16,7 @@ router.include_router(portfolio_router)
 router.include_router(transaction_router)
 router.include_router(income_router)
 router.include_router(asset_router)
+router.include_router(dashboard_router)
 
 # Just to make POC for EOD integration.
 router.include_router(eod_router)
