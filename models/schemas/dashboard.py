1a6d74fc540747d2fb2f67d33daa6c92a9a8409b
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-04 01:56:02 +0300

    Dashboard assets stats schema.

--- models/schemas/dashboard.py
+++ models/schemas/dashboard.py
@@ -5,3 +5,19 @@ from pydantic import (
 
 class DashboardTotalPurchase(BaseModel):
     total_purchase: int
+
+
+class DashboardAssetsStats(BaseModel):
+    id: int
+    code: str
+    price: float
+    amount: int
+
+    total_purchase_cost: int
+    total_market_price: int
+
+    portfolio_change: float
+    portfolio_change_p: float
+
+    change: float
+    change_p: float
